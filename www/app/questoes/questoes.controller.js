cd440271cc8232851e923b7648b161271a021d96
Tiago Giuliatte Pereira <tiagogiuliatte.p@gmail.com>
2018-06-09 15:17:07 -0300

    Última aula (4/4)
    Não deu tempo de terminar o app no curso

--- www/app/questoes/questoes.controller.js
+++ www/app/questoes/questoes.controller.js
@@ -1,16 +1,24 @@
 (function(){
 
-    function QuestoesController ($stateParams, QuestionariosService) {
+    function QuestoesController ($stateParams, $state, $timeout, $ionicScrollDelegate, QuestionariosService, MensagemService) {
         var vm = this;
         var codigoQuestionario = $stateParams.codigo;
         vm.questionario = {};
         vm.questoes = [];
         vm.indice = 0;
         vm.questaoCorrente = {};
+        vm.pontuacaoCorrente = 0;
+        vm.quantidadeQuestoes = 0;
 
         // Ao iniciar
         recuperarQuestionarioDetalhe();
         recuperarQuestoes();
+
+        // Índice
+        vm.responder = responder;
+        vm.finalizar = finalizar;
+        vm.irParaProxima = irParaProxima;
+        vm.irParaAnterior = irParaAnterior;
 
         // Funções
         function recuperarQuestionarioDetalhe(){
@@ -25,8 +33,62 @@
             .then(function() {
                 vm.questoes = QuestionariosService.questoes;
                 vm.questaoCorrente = vm.questoes[vm.indice];
+                vm.quantidadeQuestoes = vm.questoes.length;
             })
         }
+
+        function responder(){
+            if(vm.questaoCorrente.resposta == vm.questaoCorrente.opcaoSelecionada) {
+                vm.questaoCorrente.acertou = true;
+                vm.pontuacaoCorrente += vm.questaoCorrente.pontuacao;
+                MensagemService.sucesso("Aeeeeew!", "Acertou!")
+            } else {
+                vm.questaoCorrente.acertou = false;
+                vm.pontuacaoCorrente -= vm.questaoCorrente.pontuacao * 2;
+                MensagemService.erro("Ih rapaz...", "você errou!")
+            }
+            vm.questaoCorrente.respondida = true;
+            if(vm.indice + 1 == vm.quantidadeQuestoes) {
+                vm.questionario.respondido = true;
+            } else {
+                irParaProxima();
+            }
+        }
+
+        function irParaProxima() {
+            vm.indice++;
+            if(vm.indice < vm.quantidadeQuestoes) {
+                vm.questaoCorrente = vm.questoes[vm.indice];
+            }
+            $ionicScrollDelegate.scrollTop();
+        }
+
+        function finalizar(){
+            MensagemService.avisoCustom(
+                "Atenção:",
+                "Deseja mesmo finalizar?",
+                function(sim){
+                    if (sim) {
+                        $timeout (function() {
+                            MensagemService.sucesso (
+                                "Sucesso!",
+                                "O questionário foi finalizado!"
+                            );
+                            $state.go('app.questionarios');
+                        }, 300);
+                    }
+                }
+            )
+        }
+
+        function irParaAnterior() {
+            vm.indice--;
+            if(vm.indice >= 0) {
+                vm.questaoCorrente = vm.questoes[vm.indice];
+            }
+            $ionicScrollDelegate.scrollTop();  
+        }
+
     }
 
     angular.module('questionarioApp')
